b12f967cbda492642ccdee5b84c5b0dd72275ebc
root <root@daniel>
2017-04-11 08:32:51 +0200

    Debugging

--- monitor.php
+++ monitor.php
@@ -1,3 +1,20 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ */
 #!/usr/bin/php
 <?php
 error_reporting(E_ALL);
@@ -276,8 +293,10 @@ if ($db) {
 					# Intentos de conexion INCORRECTOS
 					elseif (preg_match($patron_incorrectas ,$linea)) {
 						
+						print_r($linea)
+						
 						$tmp = limpiar_linea($linea);
-
+						
 						$fecha = DateTime::createFromFormat('M d H:i:s', "$tmp[0] $tmp[1] $tmp[2]") -> format('Y-m-d H:i:s');
 							
 						# Las columnas dependen del registro
